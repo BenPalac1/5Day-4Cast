6a1ec3df8cedc112d32dd4e6be6e91a4a71a719f
BenPalac1 <ben.palacpac@gmail.com>
2024-01-19 21:29:16 -0500

    updated styling and chanegd date formate to M/dd/YYYY

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,11 +31,13 @@ function search() {
             console.log(currentDate); // "coord" in log
 
             // Current weather nested here
-            document.getElementById("currentWeather").innerHTML = '<h2 id="city"> ' + city + ' ' + currentDate + '</h2><article class="card-column"><figure class="card-block"><img src="http://openweathermap.org/img/w/' + json.weather[0].icon + '.png" alt="' + json.weather[0].description + '"><p>Temperature: ' + json.main.temp + '&#x2109;</p><p>Humidity: ' + json.main.humidity + '&percnt;</p><p>Wind: ' + json.wind.speed + ' mph</p></figure></article>'
+            document.getElementById("currentWeather").innerHTML = '<h2 id="city"> ' + city + ' ' + currentDate + '</h2><h4><article class="card-column"><figure class="card-block"><img src="http://openweathermap.org/img/w/' + json.weather[0].icon + '.png" alt="' + json.weather[0].description + '"><p>Temperature: ' + json.main.temp + '&#x2109;</p><p>Humidity: ' + json.main.humidity + '&percnt;</p><p>Wind: ' + json.wind.speed + ' mph</p></figure></article></h4>'
 
 
             // forecast query nested inside of current query
             var forecastQuery = "https://api.openweathermap.org/data/2.5/forecast?units=imperial&lat=" + lat + "&lon=" + lon + "&appid=" + APIkey;
+
+            
 
             fetch(forecastQuery).then((response) => {
                 return(response.json());
@@ -45,8 +47,18 @@ function search() {
 
                 for (var i=0; i < json.list.length; i+=8) {
                     console.log(i); // "cod" in log list of 40 objects 
+
+                    var date = new Date(json.list[i].dt_txt); //date is pulled as a timestamp
+
+                    // Get the components of the date (month, day, year)
+                    var month = date.getMonth() + 1; 
+                    var day = date.getDate();
+                    var year = date.getFullYear();
+
+                    // Format the date as "M/DD/YYYY"
+                    var formattedDate = month + '/' + day + '/' + year;
                     
-                    document.getElementById("forecastBlocks").innerHTML += '<article class="card-col-2"><figure id="day1" class="border p-3"><h3 class="header-block">' + json.list[i].dt_txt + '</h3><aside class="weather-block"><img src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png" alt="' + json.list[i].weather[0].description + '"><p>Temperature: ' + json.list[i].main.temp + '&#x2109;</p><p id="humid1">Humidity: ' + json.list[i].main.humidity + '&percnt;</p><p id="wind1">Wind: ' + json.list[i].wind.speed + ' mph</p></aside></figure></article>';
+                    document.getElementById("forecastBlocks").innerHTML += '<article class="card-col-2"><figure id="day1" class="border p-3"><h3 class="header-block">' + formattedDate + '</h3><aside class="weather-block"><img src="http://openweathermap.org/img/w/' + json.list[i].weather[0].icon + '.png" alt="' + json.list[i].weather[0].description + '"><p>Temperature: ' + json.list[i].main.temp + '&#x2109;</p><p id="humid1">Humidity: ' + json.list[i].main.humidity + '&percnt;</p><p id="wind1">Wind: ' + json.list[i].wind.speed + ' mph</p></aside></figure></article>';
                 }
             });
         });
